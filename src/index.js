c95e07be04e9090283a70891d75fb37200cc2991
RadikSaitbatalov <Sanbatalov@gmail.com>
2024-03-01 00:02:52 +0500

    Удалил import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import state from './Redux/state';
 import {createRoot} from "react-dom/client";
-
 
 const root  = createRoot(document.getElementById('root'));
 root.render (<App  state={state} />)
